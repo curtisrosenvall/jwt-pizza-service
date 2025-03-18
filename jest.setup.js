27ca2b9b35d563486d5518701d342620b524359b
boxtrain <curt.rosenvall11@gmail.com>
2025-03-18 17:55:41 -0600

    run

---
+++ jest.setup.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create the config file with database credentials
+const configContent = `
+module.exports = {
+  jwtSecret: 'test-secret',
+  db: {
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: 'theHood1335!',
+      database: 'pizza_test',
+      connectTimeout: 60000,
+    },
+    listPerPage: 10,
+  },
+  factory: {
+    url: 'https://pizza-factory.cs329.click',
+    apiKey: 'test-key',
+  },
+};
+`;
